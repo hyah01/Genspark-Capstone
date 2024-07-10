082d4e4e89e93ca91b2a8875832455852678db56
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2024-07-10 10:01:26 -0400

    Product / Promotion Repository

---
+++ BackEnd/product-service/src/main/java/com/genspark/product_service/repositories/PromotionRepository.java
@@ -0,0 +1,10 @@
+package com.genspark.product_service.repositories;
+
+import com.genspark.product_service.entities.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+
+}
