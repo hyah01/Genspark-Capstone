d07d7ed0c18eea380785b29f415c3622c01ebf30
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2024-07-19 13:29:29 -0400

    login

--- FrontEnd/amaJohn/src/app/app.module.ts
+++ FrontEnd/amaJohn/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
