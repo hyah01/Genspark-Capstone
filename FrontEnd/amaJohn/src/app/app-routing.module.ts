45f5b7261713c675396140ab056d81cb5ac94a7f
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2024-07-19 16:30:20 -0400

    sign up styling

--- FrontEnd/amaJohn/src/app/app-routing.module.ts
+++ FrontEnd/amaJohn/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:"", component: HomepageComponent},
   {path:"login", component: LoginComponent},
+  {path:"register", component: SignupComponent},
 ];
 
 @NgModule({
