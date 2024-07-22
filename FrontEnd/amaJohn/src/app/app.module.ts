b313df256e25182fb204062eae37ff2920c78af6
habioye <78454514+habioye@users.noreply.github.com>
2024-07-22 17:39:17 -0400

    added shopping and routing components

--- FrontEnd/amaJohn/src/app/app.module.ts
+++ FrontEnd/amaJohn/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -16,6 +17,8 @@ import { SignupComponent } from './signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ShoppingCartComponent,
+    AppComponent,
     NavbarComponent,
     HomepageComponent,
     SearchBarComponent,
@@ -25,6 +28,7 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule,
     FormsModule,
     CommonModule,
