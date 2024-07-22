207382d542b0a024e1f41f2c5b3f4f05950de9d9
habioye <78454514+habioye@users.noreply.github.com>
2024-07-22 17:29:49 -0400

    changed the review file to have lombook decorators for the entity

--- BackEnd/review-service/src/main/java/com/genspark/review_service/Entity/Review.java
+++ BackEnd/review-service/src/main/java/com/genspark/review_service/Entity/Review.java
@@ -2,27 +2,36 @@ package com.genspark.review_service.entity;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Document
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
     @Id
-    long reviewId;
+    String reviewId;
     long userId;
     long productId;
     String comment;
     int rating;
     Date date;
 
-    public Review(long reviewId, long userId, long productId, String comment, int rating, Date date) {
-        this.reviewId = reviewId;
-        this.userId = userId;
-        this.productId = productId;
-        this.comment = comment;
-        this.rating = rating;
-        this.date = date;
-    }
+    // public Review(long reviewId, long userId, long productId, String comment, int
+    // rating, Date date) {
+    // this.reviewId = reviewId;
+    // this.userId = userId;
+    // this.productId = productId;
+    // this.comment = comment;
+    // this.rating = rating;
+    // this.date = date;
+    // }
 
 }
